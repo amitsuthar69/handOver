f399baf3ce20344e065d24ab37637650770280ed
amit <sutharamit707@gmail.com>
2023-12-20 16:23:14 +0530

    fixes

--- app/dashboard/edit/page.tsx
+++ app/dashboard/edit/page.tsx
@@ -1,22 +1,5 @@
-export default function EditProfile() {
-  return (
-    <div className="dark font-mono text-gray-50 h-screen flex flex-col gap-2 p-8">
-      <h1 className="text-center">Edit Details</h1>
-      <form className="flex flex-col gap-4">
-        <input
-          className="edit-form-input"
-          type="text"
-          name="username"
-          placeholder="username"
-        />
-        <input
-          className="edit-form-input"
-          placeholder="phone number"
-          type="text"
-          name="phone"
-        />
-        <button className="btn-cyan">Submit</button>
-      </form>
-    </div>
-  );
+import EditProfile from "@/app/ui/dashboard/EditProfile";
+
+export default function Page() {
+  return <EditProfile />;
 }
