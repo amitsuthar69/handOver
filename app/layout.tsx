d685c9004526243f972d3ef0e12d6ac604ac7e79
amit <sutharamit707@gmail.com>
2024-01-22 21:16:13 +0530

    google-site-verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="LCJ4aDPQxgYnKh5GwKapYOw9JsZJvw3aI5_8ZBnYqG4"
+        />
+      </head>
       <body className={inter.className}>
         <NextAuthProvider>
           {children}
